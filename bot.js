ace022e746eef510e9d112edfa0cf1d5d822a516
ahhaahah <43072977+ahhaahah@users.noreply.github.com>
2018-09-08 23:40:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,22 +3,22 @@ const client = new Discord.Client();
 const prefix = '+'
 
 client.on('ready', () => {
-  console.log(Logged in as ${client.user.tag}!);
-client.user.setGame(!help,"https://www.twitch.tv/NICKMERCS%22)
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(` !help `,"https://www.twitch.tv/NICKMERCS")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log([Start] ${new Date()});
+  console.log(`[Start] ${new Date()}`);
   console.log('╚[═════════════════════════════════════════════════════════════════]╝')
   console.log('')
   console.log('╔[════════════════════════════════════]╗');
-  console.log(Logged in as * [ " ${client.user.username} " ]);
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
   console.log('')
   console.log('Informations :')
   console.log('')
-  console.log(servers! [ " ${client.guilds.size} " ]);
-  console.log(Users! [ " ${client.users.size} " ]);
-  console.log(channels! [ " ${client.channels.size} " ]);
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
   console.log('╚[════════════════════════════════════]╝')
   console.log('')
   console.log('╔[════════════]╗')
@@ -27,3 +27,87 @@ client.user.setGame(!help,"https://www.twitch.tv/NICKMERCS%22)
   console.log('')
   console.log('')
 });
+
+client.on("message", message => {
+              var args = message.content.substring(prefix.length).split(" ");
+              if (message.content.startsWith(prefix + "clear")) {
+                  if(!message.channel.guild) return message.reply('**❌ ليس لديك رتبة ادمن **');         
+     if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('**🚫  MANAGE_MESSAGES ليس لديك رتبة ادمن او بريم 🚫 **');
+          var msg;
+          msg = parseInt(); ///KillerFox AlphaCodes
+        
+        message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error); ///KillerFox AlphaCodes
+        message.channel.sendMessage("", {embed: { ///KillerFox AlphaCodes
+          title: "** ➡ تــم مسح الشات ⬅ **",
+          color: 0x06DF00,
+          footer: { ///KillerFox AlphaCodes
+            
+          }
+        }}).then(msg => {msg.delete(10000)});
+                            }
+  
+       
+  });
+
+client.on('message', message => {
+              if(!message.channel.guild) return;
+    if(message.content.startsWith('!bc')) {
+    if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
+  if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
+    let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
+    let copy = "【Creators】";
+    let request = `Requested By ${message.author.username}`;
+    if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
+    msg.react('✅')
+    .then(() => msg.react('❌'))
+    .then(() =>msg.react('✅'))
+    
+    let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
+    let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
+          let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
+    let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
+    reaction1.on("collect", r => {
+    message.channel.send(`☑ | Done ... The Broadcast Message Has Been Sent For ${message.guild.members.size} Members`).then(m => m.delete(5000));
+    message.guild.members.forEach(m => {
+    var bc = new
+       Discord.RichEmbed()
+       .setColor('RANDOM')
+       .setTitle('Broadcast')
+       .addField('Server', message.guild.name)
+       .addField('Sender', message.author.username)
+       .addField('Message', args)
+       .setThumbnail(message.author.avatarURL)
+       .setFooter(copy, client.user.avatarURL);
+    m.send({ embed: bc })
+    msg.delete();
+    })
+    })
+    reaction2.on("collect", r => {
+    message.channel.send(`**Broadcast Canceled.**`).then(m => m.delete(5000));
+    msg.delete();
+    })
+    })
+    }
+    });
+
+
+
+client.on('message', msg => {
+
+  if (msg.content === '!help') {
+
+    msg.reply('!bc و اكتب الرسالة');
+
+  }
+
+});
+
+
+
+
+
+
+
+
+
+client.login(process.env.BOT_TOKEN)
